dc6531b90135dbace335122c6b4af3ebfc5e620e
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-03-30 18:44:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ParseAndTryParse
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Program.EvenNumber(20);
+
+            Program P = new Program();
+            int Sum = P.Add(12, 23);
+
+            Console.WriteLine("Sum = {0}", Sum);
+
+        }
+
+        //Instance Method
+        public int Add(int FN, int SN)
+        {
+            return FN + SN;
+        }
+
+        //Static Method
+        public static void EvenNumber(int Target)
+        {
+            int start = 0;
+
+            while(start <= Target)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+          
+        }
+    }
+}
